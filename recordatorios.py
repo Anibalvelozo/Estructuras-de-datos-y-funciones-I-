46bb9355a629c219c444dfe8e35ab31b2dec82df
anibal <anibalvelozo2010@gmail.com>
2024-03-09 00:27:04 -0300

    Subir Recordatorios.py

--- recordatorios.py
+++ recordatorios.py
@@ -4,5 +4,33 @@ recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
  ['2021-09-18', "16:00", "Ramadas"],
  ['2021-12-25', "00:00", "Navidad"]]
 
-# Output
+#  Agregar un evento el 2 de Febrero de 2021 a las 6 de la mañana para “Empezar el Año”
+recordatorios.append(['2021-02-02', '06:00', 'Empezar el año'])
+
+#  Corregir el evento del 15 de Julio para que sea el 16 de Julio
+for evento in recordatorios:
+    if evento[0] == '2021-07-15':
+        evento[0] = '2021-07-16'
+        break
+
+# Eliminar el evento del día del trabajo
+for evento in recordatorios:
+    if evento[0] == '2021-05-01':
+        recordatorios.remove(evento)
+        break
+
+# Agregar una cena de Navidad y de Año Nuevo
+recordatorios.append(['2021-12-24', '22:00', 'Cena de Navidad'])
+recordatorios.append(['2021-12-31', '22:00', 'Cena de Año Nuevo'])
+
+# Imprimir en el formato
 print(recordatorios)
+print("---------------------------------------------------------")
+ #Imprimir en formato vertical
+for evento in recordatorios:
+    print("\n".join(evento))
+    print()  # Agregar una línea en blanco después de cada evento
+    
+ # Imprimir en formato  horizontal
+for evento in recordatorios:
+    print("["+" ".join(evento)+"]")
